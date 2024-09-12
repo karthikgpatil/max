f64400b648425a181f11dbfac66424b29668c8c1
Poorviudapudi <plu75846@gmail.comgit config --global user.name Poorviudapudigit statusgit config --global user.email plu75846@gmail.com>
2024-09-12 15:54:19 +0530

    change prog1.c

--- prog1.c
+++ prog1.c
@@ -2,4 +2,5 @@
 void main()
 {
 printf("HELLO WORLD");
+printf("welcome to git lab");
 }
---
+++ prog2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+int a,b,sum;
+printf("Enter the value of a,b");
+scanf("%d,%d",&a &b);
+sum=a+b;
+}
