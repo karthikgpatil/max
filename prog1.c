54d1b4da6030702f91121255d7b649a99f73b0c2
monikabaligeri <baligerimonika>
2024-10-21 15:45:50 +0530

    line

--- prog1.c
+++ prog1.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 void main() 
 {
+Print
 printf("Hello world");
 printf("hello lab");
 }
+i have added this line in remoterepo on saturday
+this line im adding im mylocal repo on monday
+
