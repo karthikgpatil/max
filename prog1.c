4ec135f1b245f08ac8876b7f15d7a1098e7fad84
monikabaligeri <baligerimonika@gmail.com>
2024-11-04 15:42:39 +0530

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -6,7 +6,9 @@ printf("Hello world");
 printf("hello earth");
 }
 printf("Monika");
-=======
+
 printf("Hello world")
 printf("welcome to lab")
->>>>>>> 05431f1c1b8dfe278e452d98fe2f065b451fd4ba
+
+  }
+my name is sid
